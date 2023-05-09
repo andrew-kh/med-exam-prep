60fcc08f1a5a1ded61129d0338f5b54b9d8b849d
andrew-kh <andrey.khuhlin@gmail.com>
2023-05-09 21:54:11 +0300

    demo

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import telebot
 import time
 import random
-from sqlalchemy import create_engine
+import psycopg2
 from get_env import get_env_data_as_dict
 
 env = get_env_data_as_dict('/usr/med_exam_prep/.env')
-engine = create_engine(f'postgresql+psycopg2://{env["PG_USER"]}:{env["PG_PWD"]}@127.0.0.1/{env["PG_DB"]}')
+
+conn = psycopg2.connect(
+    host="localhost",
+    database=env["PG_DB"],
+    user=env["PG_USER"],
+    password=env["PG_PWD"]
+)
 
 bot = telebot.TeleBot(env["TG_BOT_TOKEN"])
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def send_welcome(message, conn):
 	
 	bot.reply_to(message,
 	    """
@@ -22,15 +28,18 @@ def send_welcome(message):
 	
 	time.sleep(1)
 
-	with engine.connect() as conn:
-		conn.execute(f'INSERT INTO med.user_questions (user_id, question_id) VALUES ({message.chat.id}, NULL)')
+	cur = conn.cursor()
+	cur.execute(f'INSERT INTO med.user_questions (user_id, question_id) VALUES ({message.chat.id}, NULL)')
+	conn.commit()
+	cur.close()
 
 	rand_q_id = random.randint(0, 1583)
 
-	with engine.connect() as conn:
-		result = conn.execute(f'SELECT DISTINCT question_text FROM med.questions_raw WHERE question_id = {rand_q_id}').fetchone()[0]
+	cur = conn.cursor()
+	cur.execute(f'SELECT DISTINCT question_text FROM med.questions_raw WHERE question_id = {rand_q_id}')
+	question_text = cur.fetchone()[0]
 
-	bot.send_message(message.chat.id, f'Вопрос: {result}')
+	bot.send_message(message.chat.id, f'Вопрос: {question_text}')
 	
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
