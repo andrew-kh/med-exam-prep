6a761247b0e810aa798cf576d431754fa554c936
andrew-kh <andrey.khuhlin@gmail.com>
2023-05-09 21:05:59 +0300

    start work on demo

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ env = get_env_data_as_dict('/usr/med_exam_prep/.env')
 
 bot = telebot.TeleBot(env["TG_BOT_TOKEN"])
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-
+	bot.reply_to(message, """
+							Привет! Это тестовая версия бота. Сейчас я отправлю тебе случайный вопрос.\n
+							В ответ нужно прислать только номер правильного ответа\n
+							(ели ответов несколько - пришли номера без пробелов, например, 24)""")
+	bot.send_message(message.chat.id, f'Твой уникальный номер - {message.chat.id}')
+	
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
