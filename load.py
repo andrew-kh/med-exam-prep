1d110729bd8fab2ce5677e1193e4ea749587cee5
andrew-kh <andrey.khuhlin@gmail.com>
2023-05-09 17:38:07 +0300

    add load.py

---
+++ load.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from get_env import get_env_data_as_dict
+
+env = get_env_data_as_dict('/usr/med_exam_prep/.env')
+engine = create_engine(f'postgresql+psycopg2://{env["PG_USER"]}:{env["PG_PWD"]}@127.0.0.1/{env["PG_DB"]}')
+
+df = pd.read_csv('./data/exam_questions_w_answers.txt', sep='\t')
+
+df.to_sql('questions_raw', con=engine, if_exists='append', schema='med', index=False)
