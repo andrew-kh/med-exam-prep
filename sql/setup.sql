1760cc4b04f401bce220b880432d00bb277f97de
andrew-kh <andrey.khuhlin@gmail.com>
2023-05-13 11:24:21 +0300

    Create setup.sql

---
+++ sql/setup.sql
@@ -0,0 +1,23 @@
+CREATE TABLE med.questions AS
+SELECT DISTINCT
+	topic_id,
+	question_id,
+	question_text
+FROM med.questions_raw
+
+CREATE TABLE med.answers AS
+SELECT DISTINCT
+	question_id,
+	answer_id,
+	answer_text,
+	is_correct_answer
+FROM med.questions_raw
+
+CREATE TABLE active_sessions (
+	user_id INTEGER,
+	session_id SERIAL PRIMARY KEY,
+	question_id INTEGER,
+	correct_answer_id INTEGER,
+	shuffled_answer_id INTEGER,
+	is_answered INTEGER
+)
